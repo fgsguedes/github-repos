0f0ae3ba6f2476b3393bda425c5f696adc9e6301
Filipe Guedes <fgs.guedes@gmail.com>
2018-03-16 23:24:06 -0300

    Basic layout structure

---
+++ app/src/main/java/com/fgsguedes/githubrepos/ui/activity/RepositoryListActivity.kt
@@ -0,0 +1,13 @@
+package com.fgsguedes.githubrepos.ui.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.fgsguedes.githubrepos.R
+
+class RepositoryListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_repository_list)
+    }
+}
